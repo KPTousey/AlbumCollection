68187471b9f4e3849247b26e4713545c1b358b0b
JenniferJones216 <jenniferjones216@gmail.com>
2021-11-09 15:25:00 -0500

    startedsetting up the db context

---
+++ backend/album-collection/AlbumsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace album_collection
+{
+    public class AlbumsDbContext : DbContext
+    {
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			var connectionString = "Server=(localdb)\\mssqllocaldb; Database=AlbumDB_110921; Trusted_Connection=True";
+			optionsBuilder.UseSqlServer(connectionString).UseLazyLoadingProxies();
+			base.OnConfiguring(optionsBuilder);
+
+		}
+		protected override void OnModelCreating(ModelBuilder modelbuilder)
+		{
+
+		}
+	}
