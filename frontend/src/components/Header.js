519399984a5a6c8a9d6dccc11fae86cefc9b8ba8
KayleyB001 <90276649+KayleyB001@users.noreply.github.com>
2021-11-10 15:17:45 -0500

    help

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+export function Header() {
+    return `
+    <ul>
+        <li id="navHome">Home</li> 
+        <li id="navAlbum">Albums</li>
+        <li id="navArtist">Artists</li>
+    </ul>
+    `;
+}
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,9 +1,19 @@
+import Header from "../components/Header";
+
+const HeaderElement = document.getElementById("navbar");
+
 export default() => {
     Helloworld();
+    SetHeader();
+
 }
 
 function Helloworld(){
     const app = document.getElementById('app');
 
     app.innerText = "Hello World";
+}
+
+function SetHeader(){
+    HeaderElement.innerHTML = Header.Header();
 }
