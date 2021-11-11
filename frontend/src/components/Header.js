f1268caa958a9f2b4faae309dc1eb34286c84055
khmerdude <jett1004@gmail.com>
2021-11-11 14:22:50 -0500

    adding buttons

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,9 @@
-export default function Header() {
+export default {
+    SetupNavBar,
+    SetupHeaderEventListeners
+}
+
+export function SetupNavBar(){
     return `
     <ul>
         <li id="navHome">Home</li> 
@@ -8,4 +13,33 @@ export default function Header() {
         <li id="navArtist">Reviews</li>
     </ul>
     `;
+}
+
+
+export function SetupHeaderEventListeners(){
+   SetupHome();
+   SetupAlbums();
+
+}
+function SetupHome(){
+    const pageContent = document.getElementById("pageContent")
+    const btnHome = document.getElementById("navHome");
+    btnHome.addEventListener("click", function(){
+     pageContent.innerHTML = `
+     <h1>Hello World Productions</h1>
+     <p>Welcome to our album collection!</p>
+     `
+   });
+}
+
+function SetupAlbums(){
+    const btnAlbums = document.getElementById("navAlbum")
+    btnAlbums.addEventListener("click", function(){
+        console.log("hello")
+        fetch("")
+        .then(response => response.jason())
+        .then(data =>{
+            pageContent.innerHTML = Albums.DisplayAlbums(data); // only a placeholder. Need data
+        });
+    });
 }
